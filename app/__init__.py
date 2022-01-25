006f7ebf577f274315007fdc7ea5edf0b40fa40b
james-muriithi <muriithijames556@gmail.com>
2022-01-25 23:08:03 +0300

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+# Initializing application
+app = Flask(__name__)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
+
+from app import views
+from app import error
